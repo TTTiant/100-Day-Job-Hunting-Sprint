f42d224d90559cb86dfbb6671d004c3f5b0db910
Tian Zhang <tianzhang@MacBook-Pro-3.local>
2025-03-17 00:37:38 +0800

    3.16更新LRU和动态规划

---
+++ Practice_11_6/src/main/java/LRUCache.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+/*public class LRU {
+    private final int capacity;
+    private final Map<K,Node<K,V>> chche;
+    private final Node<K,V> head;
+    private final Node<K,V> tail;
+
+    public void LRUCache(int capacity){
+        this.capacity = capacity;
+        this.cache = new HashMap<>();
+        this.head = new Node<>(null, null);
+        this.tail = new Node<>(null, null);
+        head.next = tail;
+        tail.prev = head;
+
+    }
+
+
+}
+*/
+
